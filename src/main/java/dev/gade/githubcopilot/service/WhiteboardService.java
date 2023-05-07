aaaac5f5a3ed725725711010f352c1d0e8b526b4
vamshidhar <vamshig81@gmail.com>
2023-05-07 10:13:08 -0400

    'vamshidhar vamshig81@gmail.com'

--- src/main/java/dev/gade/githubcopilot/service/WhiteboardService.java
+++ src/main/java/dev/gade/githubcopilot/service/WhiteboardService.java
@@ -11,4 +11,20 @@ public class WhiteboardService {
         return str.replaceAll(subStr, "");
     }
 
+    public String removeSubString2(String str, String subStr) {
+        return str.replace(subStr, "");
+    }
+    //write a method that will return the number of words in a string
+    public int countWords(String str) {
+        return str.split(" ").length;
+    }
+
+    //write fibonacci series
+    public int fibonacci(int n) {
+        if (n <= 1) return n;
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+    //write a method that will return the number of vowels in
+
 }
